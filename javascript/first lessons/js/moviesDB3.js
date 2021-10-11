c8acf2d209c0766ac7482dc897927718b094f342
Nikita Kaem <nikitakaem@gmail.com>
2021-10-11 22:03:51 +0300

    rebase

---
+++ javascript/first lessons/js/moviesDB3.js
@@ -0,0 +1,64 @@
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. 
+Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать 
+на вопрос "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ 
+записывается в массив данных genres
+
+P.S. Функции вызывать не обязательно*/
+
+'use strict';
+
+"use strict";
+
+let numberOfFilms;
+
+
+function start() {
+    numberOfFilms = prompt('How much movies you watched?', '');
+    
+    while (numberOfFilms == '' || numberOfFilms == null || 
+    isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How much movies you watched?', '');
+    }
+}
+
+start();
+
+
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false 
+    };
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Which last film you watched?', ''),
+          b = prompt('How much rating you give for it?', '');
+    if (a.length < 50 && a.length != 0 && a != null && a != '' && b != '') {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    } 
+}
+
+
+
+if (personalMovieDB.count < 10 && personalMovieDB.count != 0) {
+    console.log('Not much films watched');
+} else if (personalMovieDB.count > 30) {
+    console.log('You are filmman');
+} else  if (10 < personalMovieDB.count < 30)  {
+    console.log('You are classic watcher');  
+} else {
+    console.log('error');
+}
+
+console.log(personalMovieDB);
